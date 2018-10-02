5e15bc97a90641833e179005ce00bd71799e69ee
Louis Riehl <louisriehl@gmail.com>
2018-10-02 16:22:09 +0000

    generate urls_index page with ejs

--- express_server.js
+++ express_server.js
@@ -18,6 +18,11 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase};
+  res.render("urls_index", templateVars); //passing urlDatabase to urls_index.ejs
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>world!</b> </body></html>");
 });
