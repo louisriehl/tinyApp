0f4fc20b03763ea310bacc4f97c3905c9c504d4d
Louis Riehl <louisriehl@gmail.com>
2018-10-02 18:35:58 +0000

    added nodemon dev dependency: initialize with npm start

--- database.js
+++ database.js
@@ -7,5 +7,6 @@ const db = [
 module.exports= {
   all: () => db,
   byShort: (sh) => db.find(e => e.short == sh), // if we find an element whose short value is the same as sh, return it
-  byLong: (lo) => db.find(e => e.long == lo) // if we find an element whose long value is the same as lo, return it
+  byLong: (lo) => db.find(e => e.long == lo), // if we find an element whose long value is the same as lo, return it
+  update: (lo, sh) => db.push({ long: lo, short: sh}) //updates db?
 };
