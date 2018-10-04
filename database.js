e4f6d18f5762629f130949414662efa65d296399
Louis Riehl <louisriehl@gmail.com>
2018-10-04 15:49:47 +0000

    integrated unique visitor into database

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const db = {
   "zPoPpHxQ": {
-    b2xVn2: { long: "http://www.lighthouselabs.ca", date: "Wed May 21 1994", visits: 0},
-    s9m5xK: { long: "http://www.google.com", date: "Mon Jan 01 2000", visits: 0 },
-    ba985d: { long: "https://www.example.com", date: "Sat Jul 18 2077", visits: 0 }
+    b2xVn2: { long: "http://www.lighthouselabs.ca", date: "Wed May 21 1994", visits: 0, unique: 0},
+    s9m5xK: { long: "http://www.google.com", date: "Mon Jan 01 2000", visits: 0, unique: 0 },
+    ba985d: { long: "https://www.example.com", date: "Sat Jul 18 2077", visits: 0, unique: 0 }
   }
 };
 
@@ -13,8 +13,10 @@ module.exports = {
   getOne: (id, short) => db[id][short]["long"],
   getDate: (id, short) => db[id][short]["date"],
   getVisits: (id, short) => db[id][short]["visits"],
+  getUniques: (id, short) => db[id][short]["unique"],
   addVisit: (id, short) => { db[id][short].visits++},
-  add: (id, short, lo, dt) => { db[id][short] = { long: lo, date: dt, visits: 0}},
+  addUnique: (id, short) => { db[id][short].unique++},
+  add: (id, short, lo, dt) => { db[id][short] = { long: lo, date: dt, visits: 0, unique: 0}},
   delete: (id, short) => { delete db[id][short]},
   owner: (short) => {
     for (let thing in db) {
