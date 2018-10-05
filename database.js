510d69f24d955bb9a3089f06657bf34a219d4bf2
Louis Riehl <louisriehl@gmail.com>
2018-10-05 16:34:22 +0000

    updating db to store tracked users

--- database.js
+++ database.js
@@ -1,8 +1,23 @@
 const db = {
   "zPoPpHxQ": {
-    b2xVn2: { long: "http://www.lighthouselabs.ca", date: "Wed May 21 1994", visits: 0, unique: 0},
-    s9m5xK: { long: "http://www.google.com", date: "Mon Jan 01 2000", visits: 0, unique: 0 },
-    ba985d: { long: "https://www.example.com", date: "Sat Jul 18 2077", visits: 0, unique: 0 }
+    b2xVn2: {
+      long: "http://www.lighthouselabs.ca",
+      date: "Wed May 21 1994",
+      visits: 0,
+      unique: 0,
+      tracking : []},
+    s9m5xK: {
+      long: "http://www.google.com",
+      date: "Mon Jan 01 2000",
+      visits: 0,
+      unique: 0,
+      tracking: []},
+    ba985d: {
+      long: "https://www.example.com",
+      date: "Sat Jul 18 2077",
+      visits: 0,
+      unique: 0,
+      tracking: []}
   }
 };
 
@@ -16,7 +31,7 @@ module.exports = {
   getUniques: (id, short) => db[id][short]["unique"],
   addVisit: (id, short) => { db[id][short].visits++},
   addUnique: (id, short) => { db[id][short].unique++},
-  add: (id, short, lo, dt) => { db[id][short] = { long: lo, date: dt, visits: 0, unique: 0}},
+  add: (id, short, lo, dt) => { db[id][short] = { long: lo, date: dt, visits: 0, unique: 0, tracking: []}},
   delete: (id, short) => { delete db[id][short]},
   owner: (short) => {
     for (let thing in db) {
